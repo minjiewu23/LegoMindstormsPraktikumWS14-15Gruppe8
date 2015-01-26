9f1d54fce499340292096f5f07538d54ae99cbda
Steffen <uddkl@student.kit.edu>
2015-01-26 20:59:11 +0100

    Changed the Clas to run straight for 5000 units

--- DrivingTests/DrivingTests/src/DrivingTests.java
+++ DrivingTests/DrivingTests/src/DrivingTests.java
@@ -1,11 +1,15 @@
 import lejos.nxt.Motor;
 import lejos.robotics.navigation.DifferentialPilot;
 
-
 public class DrivingTests {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		DifferentialPilot pilot = new DifferentialPilot(20, 100, Motor.A, Motor.B);
-		pilot.setTravelSpeed(100);
-		pilot.forward();
+		DrivingTests.travelStraight(pilot);
+	}
+	
+	public static void travelStraight(DifferentialPilot pilot) {
+		pilot.setTravelSpeed(200);
+		pilot.travel(5000);
+		pilot.stop();
 	}
 }
