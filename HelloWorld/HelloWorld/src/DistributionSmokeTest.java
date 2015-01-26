c49989a43fa600c67eb8cd2a5cc0bdab0293ebe1
Steffen <uddkl@student.kit.edu>
2015-01-26 12:19:14 +0100

    initial commit of HelloWorld example

---
+++ HelloWorld/HelloWorld/src/DistributionSmokeTest.java
@@ -0,0 +1,13 @@
+import lejos.nxt.*;
+
+public class DistributionSmokeTest
+{
+  public static void main (String[] args) throws Exception
+  {
+     LCD.clear();
+     LCD.drawString("SMOKE", 0, 0);
+     Thread.sleep(1000);
+     ///TextLCD.print ("TEST");
+     Thread.sleep(1000);
+  }
+}
