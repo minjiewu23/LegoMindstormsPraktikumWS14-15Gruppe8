106e9ce449f8e63b9fd0411d2eedf0ecbc00da31
Steffen <uddkl@student.kit.edu>
2015-01-26 21:06:17 +0100

    Added Project and class to test Sensors at this point only the motor is testet. Where is 0 Degree and how good is he reaching it

---
+++ SensorTests/SensorTests/src/SensorTests.java
@@ -0,0 +1,27 @@
+import lejos.nxt.Motor;
+import lejos.nxt.NXTRegulatedMotor;
+
+
+public class SensorTests {
+	
+	public static void main(String[] args) throws Exception {
+		SensorTests.rotateSensor();
+	}
+	
+	public static void rotateSensor() throws Exception {
+		NXTRegulatedMotor sensorMotor = Motor.C;
+		
+		//rotate Sensor To Positions (should be tested, which is the 0 Position)
+		sensorMotor.rotateTo(0);
+		Thread.sleep(1000);
+
+		sensorMotor.rotateTo(90);
+		Thread.sleep(1000);
+		
+		sensorMotor.rotateTo(180);
+		Thread.sleep(1000);
+		
+		sensorMotor.rotateTo(270);
+		Thread.sleep(1000);
+	}
+}
