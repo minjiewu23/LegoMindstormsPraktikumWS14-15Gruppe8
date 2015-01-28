99bf72da54074846a1f0cc0d77fd7289b4d41c47
Steffen <uddkl@student.kit.edu>
2015-01-28 22:26:28 +0100

    added Const Class

---
+++ Parcour1.0/Parcour1.0/util/Consts.java
@@ -0,0 +1,23 @@
+public class Consts {
+	
+	/**
+	 * LineFollower Constants
+	 */
+	
+	public static final int LINE_SEPERATION_VALUE = 40;
+	
+	public static final int LINE_SEARCH_ARC = 2;
+
+	
+	/**
+	 * BridgeRider Constants
+	 */
+	
+	public static final int BRIDGE_SEPERATION_VALUE = 30;
+	
+	public static final int BRIDGE_SEARCH_ARC = 4;
+
+	private Consts() {
+		throw new AssertionError();
+	}
+}
