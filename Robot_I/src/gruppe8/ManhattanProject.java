bd234e44b6b01067d55d06623f54d78d8fbc6657
DaryLee <DaryLee@DaryLeePC>
2015-01-28 17:06:19 +0100

    minor changes

--- Robot_I/src/gruppe8/ManhattanProject.java
+++ Robot_I/src/gruppe8/ManhattanProject.java
@@ -1,35 +1,48 @@
 package gruppe8;
 
 import lejos.nxt.LCD;
+import lejos.nxt.Motor;
 import lejos.nxt.SensorPort;
 import lejos.nxt.TouchSensor;
+import lejos.robotics.navigation.DifferentialPilot;
 
 public class ManhattanProject {
     
 	public static void main(String[] args){
 		
-		
+		DifferentialPilot pilot = new DifferentialPilot(10, 10, Motor.A, Motor.B);
 	       TouchSensor touch1 = new TouchSensor(SensorPort.S3);
 	       TouchSensor touch2 = new TouchSensor(SensorPort.S4);
 	       int count1 = 0 ;
 	       int count2 = 0;
+
+
+
+//	           if(touch1.isPressed()){
+//	        	   pilot.stop();
+//	        	   pilot.rotate(120);
+//
+//	             count1++;
+//	             LCD.drawString("Left: "+count1, 3, 3);
+//	             LCD.refresh();
+//	             while ( touch1.isPressed())
+//	                 {}
+//	              }
+//	           if(touch2.isPressed()){
+//	        	   pilot.stop();
+//	        	   pilot.rotate(-120);
+//	        	   
+//		             count2++;
+//		             LCD.drawString("Right: "+count2, 3, 4);
+//		             LCD.refresh();
+//		             while ( touch2.isPressed())
+//		                 {}
+//	           }
+//		       pilot.backward();
+	       
+	       BluetoothHandler blehandler = new BluetoothHandler();
+	       
 	     
-	           if(touch1.isPressed()){
-	             count1++;
-	             LCD.clear();
-	             LCD.drawString("Left: "+count1, 3, 3);
-	             LCD.refresh();
-	             while ( touch1.isPressed())
-	                 {}
-	              }
-	           if(touch2.isPressed()){
-		             count2++;
-		             LCD.clear();
-		             LCD.drawString("Right: "+count2, 3, 4);
-		             LCD.refresh();
-		             while ( touch2.isPressed())
-		                 {}
-	           }
 	           
 	}
 }
--- SensorTests/SensorTests/src/TouchSensor/GentleTouch.java
+++
@@ -1,5 +0,0 @@
-package TouchSensor;
-
-public class GentleTouch {
-
-}
