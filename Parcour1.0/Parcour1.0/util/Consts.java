f06bb6ed89b28adac308d10d3ba7625d052755b2
Steffen <uddkl@student.kit.edu>
2015-01-29 01:05:53 +0100

    added Constants for Motors and Sensor Ports

--- Parcour1.0/Parcour1.0/util/Consts.java
+++ Parcour1.0/Parcour1.0/util/Consts.java
@@ -1,4 +1,27 @@
+import lejos.nxt.Motor;
+import lejos.nxt.NXTRegulatedMotor;
+import lejos.nxt.SensorPort;
+
 public class Consts {
+	
+	/**
+	 * general Constants
+	 */
+	
+	public static final NXTRegulatedMotor LEFT_MOTOR = Motor.A;
+
+	public static final NXTRegulatedMotor RIGHT_MOTOR = Motor.B;
+	
+	public static final NXTRegulatedMotor SENSOR_MOTOR = Motor.C;
+	
+	
+	public static final SensorPort LIGHT_SENSOR_PORT = SensorPort.S1;
+
+	public static final SensorPort ULTRASONIC_SENSOR_PORT = SensorPort.S2;
+
+	public static final SensorPort LEFT_TOUCH_SENSOR_PORT = SensorPort.S3;
+
+	public static final SensorPort RIGHT_TOUCH_SENSOR_PORT = SensorPort.S4;
 	
 	/**
 	 * LineFollower Constants
