71395c13e0a3b4d34d1eda9cfa10c1b3fc015914
minjiewu23 <wuminjie23@googlemail.com>
2015-01-30 16:14:29 +0100

    Update Variables.java

--- Bluetooth/src/bluetooth/Variables.java
+++ Bluetooth/src/bluetooth/Variables.java
@@ -4,7 +4,7 @@ package bluetooth;
  */
 public class Variables {
 	
-	public static final String GATE_BRICK_NAME = "gate";
+	public static final String GATE_BRICK_NAME = "TestName";
 	public static final int COMMAND_CLOSE_CONNECTION = 1337;
 
 }
